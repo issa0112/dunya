2f1c457f78ad2a7984a0de476d226d47eebeed5e
dunya <issatraore0112@gmail.com>
2025-10-08 01:00:26 +0000

    Fix build.sh for Render

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Installer les dépendances Python
+# Installe les dépendances
 pip install -r requirements.txt
 
-# Collecter les fichiers statiques
+# Rassemble les fichiers statiques
 python manage.py collectstatic --no-input
 
-# Appliquer les migrations à la base de données
+# Applique les migrations
 python manage.py migrate
+
